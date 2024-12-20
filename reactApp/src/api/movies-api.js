bc2b83aad367d7733f3808982c969cb1faf2e4db
Levi <20109223@mail.wit.ie>
2024-12-20 21:05:05 +0000

    Integration Lab - Integrate Users Endpoints

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -3,4 +3,26 @@ export const getMovies = async () => {
       `https://api.themoviedb.org/3/discover/movie?api_key=07810031a3fd12224e25987cc0b2985e&language=en-US&include_adult=false&page=1`
     )
     return response.json()
-  };
+  }
+
+export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
